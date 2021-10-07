45989c8c3371d059fd7ff1891e1fe9c1a6bdf12b
Petr Hodina <phodina@protonmail.com>
2021-10-07 23:45:14 +0200

    Direnv example repo

---
+++ channels.scm
@@ -0,0 +1,20 @@
+(list (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (commit
+          "26119328aac147d2ef5bd7fd1466f113a3184db9")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        (commit
+          "f5cffdf94888351fb2e077e31878ddc53072d210")
+        (introduction
+          (make-channel-introduction
+            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+            (openpgp-fingerprint
+              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5")))))
